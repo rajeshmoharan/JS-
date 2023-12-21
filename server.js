d525726e7cb50114c0da854e1e31d7986a6569dd
rajeshmoharan <rajeshmoharana230@gmail.com>
2023-12-21 22:31:46 +0530

    Javascript document

---
+++ server.js
@@ -0,0 +1,19 @@
+
+// console.log("Rajesh JavaScript Master");
+// "use strict";
+
+const accountId = 1235
+console.log(accountId)
+
+let accountEmail = "rajesh@gmail.com"
+let accountPassword ="rajesh@"
+
+console.table([accountId,accountEmail,accountPassword])
+
+// boolean value => true or false
+// bigint
+// undefined
+// Integer
+// null
+
+// object 
